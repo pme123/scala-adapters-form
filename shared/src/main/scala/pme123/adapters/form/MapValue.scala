0771b0f6f4f58a93d0d48a7d39f9e1783e30eb80
pme123 <feensturm@drachensegler.ch>
2018-04-18 08:56:54 +0200

    changed encoder to Var / Vars

--- shared/src/main/scala/pme123/adapters/form/MapValue.scala
+++ shared/src/main/scala/pme123/adapters/form/MapValue.scala
@@ -3,42 +3,60 @@ package pme123.adapters.form
 import com.thoughtworks.binding.Binding.{Var, Vars}
 
 sealed trait MapValue {
-  def valueAsVar[A](ident: String): Var[A] = throw new UnsupportedOperationException("valueAsVar is only supported for MapObjects")
+  def varValue[A](key: ObjectKey): Var[A] = this match {
+    case MapObject(fields) =>
+      fields.get(key) match {
+        case Some(sv: SimpleValue[A]) => sv.elem
+        case _ => throw MapException(s"There is no SimpleValue for $key")
+      }
+    case other => throw MapException(s"This needs to be a MapObject (not: $other")
+  }
+
+  def varsValue[A](key: ObjectKey): Vars[A] =this match {
+    case MapObject(fields) =>
+      fields.get(key) match {
+        case Some(ml: MapList[A]) => ml.elems
+        case _ => throw MapException(s"There is no MapList for $key")
+      }
+    case other => throw MapException(s"This needs to be a MapObject (not: $other")
+  }
 }
 
-case class MapObject(fields: List[(String, MapValue)]) extends MapValue {
-  override def valueAsVar[A](ident: String): Var[A] =
-    Var(
-      fields.find(_._1 == ident)
-        .map(_._2)
-        .map(_.asInstanceOf[SimpleValue[A]])
-        .map(_.value)
-        .getOrElse(throw new IllegalArgumentException(s"There is no value for $ident"))
-    )
+case class MapObject(fields: Map[ObjectKey, MapValue]) extends MapValue {
+
 }
 
-case class MapList[A](items: Seq[SimpleValue[A]]) extends MapValue {
-  def asVars: Vars[A] = Vars[A](items.map(_.value): _*)
+case class MapList[A](elems: Vars[A]) extends MapValue {
+
+  def setValues(newValues: Seq[A]) {
+    elems.value.clear()
+    elems.value ++= newValues
+  }
+
+  def addValue(newValue: A) {
+    elems.value += newValue
+  }
 }
 
 sealed trait SimpleValue[A]
   extends MapValue {
-  def value: A
+  def elem: Var[A]
 
-  def asVar: Var[A] = Var(value)
-
+  def setValue(newValue: A) {
+    elem.value = newValue
+  }
 }
 
-case class MapString(value: String) extends SimpleValue[String] {
+case class MapString(elem: Var[String]) extends SimpleValue[String] {
 }
 
-case class MapInt(value: Int) extends SimpleValue[Int] {
+case class MapInt(elem: Var[Int]) extends SimpleValue[Int] {
 }
 
-case class MapDouble(value: Double) extends SimpleValue[Double] {
+case class MapDouble(elem: Var[Double]) extends SimpleValue[Double] {
 }
 
-case class MapBoolean(value: Boolean) extends SimpleValue[Boolean] {
+case class MapBoolean(elem: Var[Boolean]) extends SimpleValue[Boolean] {
 }
 
 case object MapNull extends MapValue
